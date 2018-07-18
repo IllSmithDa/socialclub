b95a791aa4a6652b4627ea5f1a2f00aa2288f5b2
Samuel Kim <sams404link@gmail.com>
2018-07-18 11:18:13 -0700

    removed backend stuff

--- socialclub-api/routes/routes.js
+++
@@ -1,8 +0,0 @@
-const AccountController = require('../controllers/AccountController');
-
-module.exports = (app) => {
-  app.route('/userCreate')
-    .post(AccountController.createUser)
-  app.route('/loginUser')
-    .post(AccountController.loginUser)
-}
